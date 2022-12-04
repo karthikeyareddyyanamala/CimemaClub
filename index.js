835e6a927962c364c9eee6b8c11cc902994104bf
karthikeyareddyyanamala <96631771+karthikeyareddyyanamala@users.noreply.github.com>
2022-12-04 23:42:13 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -12,11 +12,59 @@ menu.addEventListener("click", ()=> {
 //---------------------------------------Nav End-----------------------------------------------//
 
 //------------------------Home Link Button -------------------------------------------->
-
-const home_button = document.querySelector(".HomeButton");
+try{
+    const home_button = document.querySelector(".HomeButton");
 
 home_button.addEventListener("click", ()=>{
     location.href="./index.html";
 })
+}
+catch(err){
+    const login = document.querySelector(".LoginButton");
 
-//------------------------------------------------------------------------------------->
+    let login_form = document.querySelector(".Login-form");
+
+    login.addEventListener("click", ()=>{
+    login_form.classList.toggle("login-active");
+})
+}
+
+
+//----------------------------Signup overlay--------------------------------------------------------->
+const signup = document.querySelector(".Button-Signup-nav");
+const signup_form = document.querySelector(".singup-form") 
+
+signup.addEventListener("click", ()=>{
+    signup_form.classList.toggle("form-active");
+})
+
+
+const close_signup = document.querySelector(".close-button");
+
+close_signup.addEventListener("click", ()=>{
+    signup_form.classList.remove("form-active")
+})
+//---------------------------------END------------------------------------------------------------------
+
+
+
+const close_login = document.querySelector(".login-close-button");
+const login_form = document.querySelector(".Login-form");
+
+close_login.addEventListener("click", ()=>{
+    login_form.classList.remove("login-active");
+})
+
+//----------------------------------------------------------------------------------------
+
+const signuplink_overlay = document.querySelector(".Signuplink-overlay");
+
+signuplink_overlay.addEventListener("click", ()=>{
+    signup_form.classList.toggle("form-active");
+})
+
+const Loginlink_overlay = document.querySelector(".Loginlink-overlay");
+Loginlink_overlay.addEventListener("click", ()=>{
+    login_form.classList.toggle("login-active");
+})
+//-----------------------------------------------------------------------------------
